8e2fafcd300a364b09bf978e14b0297e71d918e5
孟德斌 <mengdebin@edaijia-inc.cn>
2018-05-18 10:51:57 +0800

    u

---
+++ hdata-es/src/main/java/com/github/stuxuhai/hdata/plugin/reader/es/EsReader.java
@@ -0,0 +1,12 @@
+package com.github.stuxuhai.hdata.plugin.reader.es;
+
+import com.github.stuxuhai.hdata.api.Reader;
+import com.github.stuxuhai.hdata.api.Splitter;
+
+public class EsReader extends Reader {
+
+    @Override
+    public Splitter newSplitter() {
+        return null;
+    }
+}
