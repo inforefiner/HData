1b066e190c1f41711cbe146b7c9d5de0b1d4bea3
joeyhacker <jhacker7@gmail.com>
2018-05-18 16:20:36 +0800

    u

---
+++ hdata-es/src/main/java/com/github/stuxuhai/hdata/plugin/reader/es/EsReaderProperties.java
@@ -0,0 +1,17 @@
+package com.github.stuxuhai.hdata.plugin.reader.es;
+
+public class EsReaderProperties {
+
+    public static final String IP_ADDRESSES = "ip.addresses";
+
+    public static final String CLUSTER_NAME = "cluster.name";
+
+    public static final String INDEX = "index";
+
+    public static final String INDEX_TYPE = "index.type";
+
+    public static final String FILTER = "filter";
+
+    public static final String COLUMNS = "columns";
+
+}
