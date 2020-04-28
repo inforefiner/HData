7fb166feed1fb96fa44dce5a632e856aff67d537
wuguofeng <guofeng.wu@inforefiner.com>
2020-04-28 14:05:49 +0800

    fix fileservice multi-cluster bug

--- hdata-ftp/src/main/java/com/github/stuxuhai/hdata/ftp/HdfsUtil.java
+++ hdata-ftp/src/main/java/com/github/stuxuhai/hdata/ftp/HdfsUtil.java
@@ -5,41 +5,60 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 public class HdfsUtil {
+    private final Logger logger = LoggerFactory.getLogger(HdfsUtil.class);
 
-    public static HdfsUtil getInstance(String path) {
-        return new HdfsUtil(path);
+    public static HdfsUtil getInstance(String path, String descDir) {
+        return new HdfsUtil(path, descDir);
     }
 
     private FileSystem fs;
 
-    private HdfsUtil(String path) {
-        try {
-            Configuration configuration = new Configuration();
-            if (StringUtils.isNotBlank(path)) {
-                try (ZipFile zipFile = new ZipFile(path)) {
-                    Enumeration<? extends ZipEntry> zipEntryEnumeration = zipFile.entries();
-                    if (zipEntryEnumeration.hasMoreElements()) {
-                        ZipEntry zipEntry = zipEntryEnumeration.nextElement();
-                        if (zipEntry.getName().endsWith(".xml")) {
-                            configuration.addResource(zipFile.getInputStream(zipEntry), zipEntry.getName());
-                        }
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    File file = new File(path);
-                    Runtime.getRuntime().addShutdownHook(new Thread(() -> file.delete()));
-                }
+    private HdfsUtil(String path, String descDir) {
 
+        try {
+            unZipFiles(new File(path), descDir);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            Configuration configuration = new Configuration(false);
+            if (StringUtils.isNotBlank(path)) {
+                ArrayList<File> fileList = new ArrayList<>();
+                getFiles(descDir, fileList);
+                for (File file : fileList) {
+                    if (file.getName().endsWith(".xml")) {
+                        InputStream in = new FileInputStream(file);
+                        configuration.addResource(in);
+                        logger.info("add resource:" + file.getName());
+                    }
+                }
+//                logger.info("hadoop conf path is:" + path);
+//                try (ZipFile zipFile = new ZipFile(path)) {
+//                    BufferedInputStream bis = new BufferedInputStream(new FileInputStream(path));
+//                    ZipInputStream zis = new ZipInputStream(bis);
+//
+//                    ZipEntry zipEntry;
+//                    while ((zipEntry = zis.getNextEntry()) != null) {
+//                        if (zipEntry.getName().endsWith(".xml")) {
+//                            configuration.addResource(zipFile.getInputStream(zipEntry), zipEntry.getName());
+//                            logger.info("add resource:" + zipEntry.getName());
+//                        }
+//                    }
+//                    zis.closeEntry();
+//                    bis.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
             } else {
                 String CONF_PATH = System.getenv("HADOOP_CONF_DIR");
                 if (StringUtils.isNotBlank(CONF_PATH)) {
@@ -47,7 +66,9 @@ public class HdfsUtil {
                     configuration.addResource(new Path(CONF_PATH + File.separator + "hdfs-site.xml"));
                 }
             }
+            logger.info("fs site:" +configuration.get("fs.defaultFS"));
             this.fs = FileSystem.get(configuration);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -101,4 +122,60 @@ public class HdfsUtil {
             }
         }
     }
+
+    public void unZipFiles(File zipFile, String descDir) throws IOException {
+        File pathFile = new File(descDir);
+        if (!pathFile.exists()) {
+            pathFile.mkdirs();
+        }
+        //解决zip文件中有中文目录或者中文文件
+        ZipFile zip = new ZipFile(zipFile);
+        for (Enumeration entries = zip.entries(); entries.hasMoreElements(); ) {
+            ZipEntry entry = (ZipEntry) entries.nextElement();
+            String zipEntryName = entry.getName();
+            InputStream in = zip.getInputStream(entry);
+            String outPath = (descDir + zipEntryName).replaceAll("\\*", "/");
+            ;
+            //判断路径是否存在,不存在则创建文件路径
+            File file = new File(outPath.substring(0, outPath.lastIndexOf('/')));
+            if (!file.exists()) {
+                file.mkdirs();
+            }
+            //判断文件全路径是否为文件夹,如果是上面已经上传,不需要解压
+            if (new File(outPath).isDirectory()) {
+                continue;
+            }
+            //输出文件路径信息
+            logger.info("unzip file:" + outPath);
+            OutputStream out = new FileOutputStream(outPath);
+            byte[] buf1 = new byte[1024];
+            int len;
+            while ((len = in.read(buf1)) > 0) {
+                out.write(buf1, 0, len);
+            }
+            in.close();
+            out.close();
+        }
+        logger.info("unzip successfully");
+    }
+
+    public void getFiles(String path, ArrayList<File> list) {
+        //目标集合fileList
+        File file = new File(path);
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            for (File fileIndex : files) {
+                //如果这个文件是目录，则进行递归搜索
+                if (fileIndex.isDirectory()) {
+                    getFiles(fileIndex.getPath(), list);
+                } else {
+                    //如果文件是普通文件，则将文件句柄放入集合中
+                    list.add(fileIndex);
+                }
+            }
+        }
+    }
+
+
+
 }
